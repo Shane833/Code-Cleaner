bbdae1092345fad5a49d3a4af8ce4573c3223c53
Shane833 <williamsshane883@gmail.com>
2026-03-01 16:17:04 +0530

    made some progress in the lexer

--- CodeCleaner/lexer.py
+++ CodeCleaner/lexer.py
@@ -7,43 +7,68 @@
 from enum import Enum
 
 class Token(Enum):
-	EMPTY_LINE = 1
-	SINGLE_COMM = 2
-	MULTI_LINE_COMM_START = 3
-	MULTI_LINE_COMM_END = 4
+    EMPTY_LINE = 1
+    SINGLE_COMM = 2
+    MULTI_LINE_COMM_START = 3
+    MULTI_LINE_COMM_END = 4
+    STRING_START = 5 # Adding this bcz we don't want to erase comments inside strings
+    STRING_END = 6 # can span over multiple lines
 
-# Utility function for identifying the various tokens
+class Lexer(object):
 
-# Making a class for differnt types of languages
-def is_empty_line(line) -> bool:
-	'''go through the line and determines if its just for formatting'''
-	characters = ['\n',' ','\t']
-	for l in line:
-		if l not in characters:
-			return False
-		else:
-			pass
-	return True
+    class State(Enum):
+        DEFAULT = 1
+        IN_STRING = 2
+        IN_MULTI_LINE_COMM = 3
+        
+    def __init__(self):
+        self.state = Lexer.State.DEFAULT # used to monitor when inside string we ignore scanning
 
-# Providing an additional argument in identifying the comment characters
-# so that the same function can be used for different language comments
-def has_single_comm(line, single_comm_str) -> int:
-	'''if the single line comment is found then simply
-	   return the index within the line else returns -1'''
+    def is_empty_line(self,line) -> bool:
+        '''go through the line and determines if its just for formatting'''
+        return line.strip() == "" # simple way, i.e. after removing all the whitespace if we are left with nothing then its an empty line
 
-	index = line.find(single_comm_str)
-	return index
+    # Providing an additional argument in identifying the comment characters
+    # so that the same function can be used for different language comments
+    def has_single_comm(self, line, marker) -> int:
+        '''if the single line comment is found then simply
+           return the index within the line else returns -1'''
+        return line.find(marker)
 
-def has_multi_line_comm_start(line, multi_comm_start_str) -> int:
-	''' If contains /* simply return the index else -1 '''
-	index = line.find(multi_comm_start_str)
-	return index
+    def has_multi_line_comm_start(self, line, marker) -> int:
+        ''' If contains /* simply return the index else -1 '''
+        return line.find(marker)
 
-def has_multi_line_comm_end(line, multi_comm_end_str) -> int:
-	''' If contains */ simply return the index else -1'''
-	index = line.find(multi_comm_end_str)
-	return index
+    def has_multi_line_comm_end(self, line, marker) -> int:
+        ''' If contains */ simply return the index else -1'''
+        return line.find(marker)
 
+    def has_string(self, line,marker):
+        ''' If contains "" simply return the index else -1 '''
+        return line.find(marker)
 
+    def scanline(self, line) -> list[tuple[int,Token]]:
+        ''' Go through the line and indentify all the tokens '''
+        index = 0
+        result = [] # list of tokens
 
+        # If empty line don't bother
+        if self.is_empty_line(line):
+            result.append((index, Token.EMPTY_LINE))
+        else:
+            index = self.has_single_comm(line,'//')
+            if index != -1:
+                result.append((index, Token.SINGLE_COMM))
+            
+            index = self.has_multi_line_comm_start(line, '/*')
+            if index != -1:
+                result.append((index, Token.MULTI_LINE_COMM_START))
+            
+            index = self.has_multi_line_comm_end(line, '*/')
+            if index != -1:
+                result.append((index, Token.MULTI_LINE_COMM_END))
 
+        return result
+
+    def scanfile(self, file):
+        pass
--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,43 +1,61 @@
 import pytest
 from CodeCleaner.lexer import *
 
+# creating an object of lex
+lex = Lexer()
+
 def test_empty_line():
-	lines = ["\t\n","int a","      ", "			{"]
-	assert is_empty_line(lines[0]) == True
-	assert is_empty_line(lines[1]) == False
-	assert is_empty_line(lines[2]) == True
-	assert is_empty_line(lines[3]) == False
+    lines = ["\t\n","int a","      ", "         {"]
+    assert lex.is_empty_line(lines[0]) == True   
+    assert lex.is_empty_line(lines[1]) == False
+    assert lex.is_empty_line(lines[2]) == True
+    assert lex.is_empty_line(lines[3]) == False
 
 def test_single_comm():
-	lines = ["//","int a; // This is a variable","\t\t\n","// * Hey // there\t\t",
-			 "/* // */","#include <stdio.h>\n"]
-	assert has_single_comm(lines[0],'//') != -1
-	assert has_single_comm(lines[1],'//') != -1
-	assert has_single_comm(lines[2],'//') == -1
-	assert has_single_comm(lines[3],'//') != -1
-	assert has_single_comm(lines[4],'//') != -1
-	assert has_single_comm(lines[5],'//') == -1
+    lines = ["//","int a; // This is a variable","\t\t\n","// * Hey // there\t\t",
+             "/* // */","#include <stdio.h>\n"]
+    assert lex.has_single_comm(lines[0],'//') != -1
+    assert lex.has_single_comm(lines[1],'//') != -1
+    assert lex.has_single_comm(lines[2],'//') == -1
+    assert lex.has_single_comm(lines[3],'//') != -1
+    assert lex.has_single_comm(lines[4],'//') != -1
+    assert lex.has_single_comm(lines[5],'//') == -1
 
 def test_multi_line_comm_start():
-	lines = ["/**/","float b; /* multi line comm","// /* // */",
-			 "int main{/* This is the main function */","/* */ //",
-			 "\t\t\n","int a"]
-	assert has_multi_line_comm_start(lines[0],"/*") != -1
-	assert has_multi_line_comm_start(lines[1],"/*") != -1
-	assert has_multi_line_comm_start(lines[2],"/*") != -1
-	assert has_multi_line_comm_start(lines[3],"/*") != -1
-	assert has_multi_line_comm_start(lines[4],"/*") != -1
-	assert has_multi_line_comm_start(lines[5],"/*") == -1
-	assert has_multi_line_comm_start(lines[6],"/*") == -1
+    lines = ["/**/","float b; /* multi line comm","// /* // */",
+             "int main{/* This is the main function */","/* */ //",
+             "\t\t\n","int a"]
+    assert lex.has_multi_line_comm_start(lines[0],"/*") != -1
+    assert lex.has_multi_line_comm_start(lines[1],"/*") != -1
+    assert lex.has_multi_line_comm_start(lines[2],"/*") != -1
+    assert lex.has_multi_line_comm_start(lines[3],"/*") != -1
+    assert lex.has_multi_line_comm_start(lines[4],"/*") != -1
+    assert lex.has_multi_line_comm_start(lines[5],"/*") == -1
+    assert lex.has_multi_line_comm_start(lines[6],"/*") == -1
 
 def test_multi_line_comm_end():
-	lines = ["/**/","float b; /* multi line comm","// /* // */",
-			 "int main{/* This is the main function */","/* */ //",
-			 "\t\t\n","int a"]
-	assert has_multi_line_comm_end(lines[0],"*/") != -1
-	assert has_multi_line_comm_end(lines[1],"*/") == -1
-	assert has_multi_line_comm_end(lines[2],"*/") != -1
-	assert has_multi_line_comm_end(lines[3],"*/") != -1
-	assert has_multi_line_comm_end(lines[4],"*/") != -1
-	assert has_multi_line_comm_end(lines[5],"*/") == -1
-	assert has_multi_line_comm_end(lines[6],"*/") == -1
+    lines = ["/**/","float b; /* multi line comm","// /* // */",
+             "int main{/* This is the main function */","/* */ //",
+             "\t\t\n","int a"]
+    assert lex.has_multi_line_comm_end(lines[0],"*/") != -1
+    assert lex.has_multi_line_comm_end(lines[1],"*/") == -1
+    assert lex.has_multi_line_comm_end(lines[2],"*/") != -1
+    assert lex.has_multi_line_comm_end(lines[3],"*/") != -1
+    assert lex.has_multi_line_comm_end(lines[4],"*/") != -1
+    assert lex.has_multi_line_comm_end(lines[5],"*/") == -1
+    assert lex.has_multi_line_comm_end(lines[6],"*/") == -1
+
+def test_scanline():
+    line = "\t \n"
+    result = lex.scanline(line)
+    assert result == [(0, Token.EMPTY_LINE)]
+
+    line = "/**/"
+    result = lex.scanline(line)
+    assert result == [(0, Token.MULTI_LINE_COMM_START), (2, Token.MULTI_LINE_COMM_END)]
+
+    line = "// /* */"
+    result = lex.scanline(line)
+    assert result == [(0, Token.SINGLE_COMM),
+                      (3,Token.MULTI_LINE_COMM_START),
+                      (6, Token.MULTI_LINE_COMM_END)]
